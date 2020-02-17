22ebdf569f2f23f4615c8d6871c146c035242a85
Leo Li <haohao1331@Leos-MacBook-Pro.local>
2020-02-17 00:38:16 -0500

    complete

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,6 +1,49 @@
 import React from 'react';
 import Gallery from 'react-photo-gallery';
 import pic1 from '../Image/pic1.jpg';
+import a2212178919710 from '../Image/a2212178919710.jpg';
+import a1273191254410 from '../Image/a1273191254410.jpg';
+import a778980913914 from '../Image/a778980913914.jpg';
+import a4350166060596 from '../Image/a4350166060596.jpg';
+import a3700808291016 from '../Image/a3700808291016.jpg';
+import a1895362078355 from '../Image/a1895362078355.jpg';
+import a6121001482342 from '../Image/a6121001482342.jpg';
+import a7249186614375 from '../Image/a7249186614375.jpg';
+import a1161062767369 from '../Image/a1161062767369.jpg';
+import a1243217625874 from '../Image/a1243217625874.jpg';
+import a1549648776535 from '../Image/a1549648776535.jpg';
+import a2372850080277 from '../Image/a2372850080277.jpg';
+import a6932901298667 from '../Image/a6932901298667.jpg';
+import a3849183697664 from '../Image/a3849183697664.jpg';
+import a3131726338521 from '../Image/a3131726338521.jpg';
+import a4491133493805 from '../Image/a4491133493805.jpg';
+import a3979554512804 from '../Image/a3979554512804.jpg';
+import a2572362017254 from '../Image/a2572362017254.jpg';
+import a4858525363406 from '../Image/a4858525363406.jpg';
+import a7864599558681 from '../Image/a7864599558681.jpg';
+import a179410618580 from '../Image/a179410618580.jpg';
+import a5820114635870 from '../Image/a5820114635870.jpg';
+import a1752732027017 from '../Image/a1752732027017.jpg';
+import a813650490420 from '../Image/a813650490420.jpg';
+import a5619435754513 from '../Image/a5619435754513.jpg';
+import a3302691540423 from '../Image/a3302691540423.jpg';
+import a3974794011948 from '../Image/a3974794011948.jpg';
+import a3395080870548 from '../Image/a3395080870548.jpg';
+import a2461474221779 from '../Image/a2461474221779.jpg';
+import a121679336374 from '../Image/a121679336374.jpg';
+import a3336656211648 from '../Image/a3336656211648.jpg';
+import a8810115362390 from '../Image/a8810115362390.jpg';
+import a4145322503664 from '../Image/a4145322503664.jpg';
+import a6024857308941 from '../Image/a6024857308941.jpg';
+import a7019725654506 from '../Image/a7019725654506.jpg';
+import a7710536711682 from '../Image/a7710536711682.jpg';
+import a544350659305 from '../Image/a544350659305.jpg';
+import a8074338848908 from '../Image/a8074338848908.jpg';
+import a1191603326434 from '../Image/a1191603326434.jpg';
+import a1828931582127 from '../Image/a1828931582127.jpg';
+import a562292712424 from '../Image/a562292712424.jpg';
+import a5812996143712 from '../Image/a5812996143712.jpg';
+import a3329981753503 from '../Image/a3329981753503.jpg';
 
 
 
@@ -17,6 +60,221 @@ export default class Sample extends React.Component {
 const PHOTO_SET = [
   {
     src: pic1,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a2212178919710,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a1273191254410,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a778980913914,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a4350166060596,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a3700808291016,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a1895362078355,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a6121001482342,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a7249186614375,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a1161062767369,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a1243217625874,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a1549648776535,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a2372850080277,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a6932901298667,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a3849183697664,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a3131726338521,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a4491133493805,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a3979554512804,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a2572362017254,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a4858525363406,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a7864599558681,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a179410618580,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a5820114635870,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a1752732027017,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a813650490420,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a5619435754513,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a3302691540423,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a3974794011948,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a3395080870548,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a2461474221779,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a121679336374,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a3336656211648,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a8810115362390,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a4145322503664,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a6024857308941,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a7019725654506,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a7710536711682,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a544350659305,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a8074338848908,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a1191603326434,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a1828931582127,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a562292712424,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a5812996143712,
+    width: 4,
+    height: 3
+  },
+  {
+    src: a3329981753503,
     width: 4,
     height: 3
   },
